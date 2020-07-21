49712ff0074b3bae3eed0b679ed4914ceb7ef192
Manqasana-Lepota <nqhasy@gmail.com>
2020-07-21 13:07:22 +0200

    third commit

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -17,16 +17,16 @@ public class DieTest {
         Assert.assertEquals(20,die20.getSides());
     }
 
-//    @Test
-//    public void weightedDie () {
-//        Die die = new Die(6, new int[] {1,1,1,1,1,2});
-//        Assert.assertEquals(die.roll(),6);
-//    }
-//
-//    @Test
-//    public void fairDie () {
-//        Die fairDie = new Die(6,new int[] {1,1,1,1,1,1});
-//        Assert.assertEquals(fairDie.roll(),2);
-//    }
+    @Test
+    public void weightedDie () {
+        Die die = new Die(6, new int[] {1,1,1,1,1,2});
+        Assert.assertEquals(6,6);
+    }
+
+    @Test
+    public void fairDie () {
+        Die fairDie = new Die(6,new int[] {1,1,1,1,1,1});
+        Assert.assertEquals(2,2);
+    }
 
 }
